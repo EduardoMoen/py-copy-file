6ab7d97eb1141d2a5f5a13b77c4380e77d4ccf06
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-10 15:15:38 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+    rep_command = command.split(" ")
+    if len(rep_command) != 3:
+        return
+
+    cp, source_file, new_file = rep_command
+
+    if cp != "cp":
+        return
+
+    if source_file == new_file:
+        return
+
+    try:
+        with (open(source_file, "r") as file_in,
+              open(new_file, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print("File not exists.")
